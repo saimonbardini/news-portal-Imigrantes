95ee3b395fb02577cf0c393e9b4ad66da0f7968f
Saimon Bardini <saimonmoncani@gmail.com>
2026-06-04 13:28:14 -0300

    Feature: Implements a custom WordPress logo using Tailwind classes in the header.

--- functions.php
+++ functions.php
@@ -10,6 +10,12 @@ if ( ! function_exists( 'radio_news_theme_setup' ) ) :
 		add_theme_support( 'title-tag' );
 		add_theme_support( 'post-thumbnails' );
 
+		// Adiciona suporte a logo personalizada
+		add_theme_support( 'custom-logo', array(
+			'flex-height' => true,
+			'flex-width'  => true,
+		) );
+
 		register_nav_menus(
 			array(
 				'menu-1' => esc_html__( 'Primary', 'radio-news-theme' ),
@@ -48,3 +54,13 @@ function radio_news_theme_scripts() {
 	wp_enqueue_script( 'radio-news-theme-main', get_template_directory_uri() . '/assets/js/main.js', array('swup'), '1.0.0', true );
 }
 add_action( 'wp_enqueue_scripts', 'radio_news_theme_scripts' );
+
+/**
+ * Adiciona classes do Tailwind à logo personalizada do painel
+ */
+function radio_news_custom_logo_classes( $html ) {
+	$html = str_replace( 'class="custom-logo-link"', 'class="custom-logo-link flex items-center"', $html );
+	$html = str_replace( 'class="custom-logo"', 'class="custom-logo h-10 md:h-12 w-auto object-contain"', $html );
+	return $html;
+}
+add_filter( 'get_custom_logo', 'radio_news_custom_logo_classes' );
